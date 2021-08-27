165f845adc9a3a2c4efd5faac70ae94d297356a5
Teaca Sebastian <sebiteaca@gmail.com>
2021-08-27 17:52:22 +0300

    finished mock api

--- server/test-api/server.js
+++ server/test-api/server.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const path = require('path')
+const cors = require('cors')
 
 app = express()
 
 const PORT = 3000
+
+app.use(cors())
+
+app.get('/plans', (req, res) => {
+    res.sendFile(path.join(__dirname, 'plans.json'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
